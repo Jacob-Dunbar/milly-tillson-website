b140eee387354bd93bc9b712bf08978c528e4c92
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-12-16 18:36:29 +0000

    feat: add gallery page

--- components/GalleryCard.tsx
+++ components/GalleryCard.tsx
@@ -1,22 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
-import css from "../styles/GalleryCard.module.scss";
+import css from "../styles/Card.module.scss";
 
 const GalleryCard = ({ gallery, image }: any) => {
   const { name, order, slug } = gallery.fields;
   console.log(image.fields.file.url);
   return (
     <Link href={"/galleries/" + slug}>
-      <div className={css.galleryCard}>
+      <div className={css.card}>
         <Image
-          className={css.galleryCard__image}
+          className={css.card__image}
           src={`https:${image.fields.file.url}`}
           quality={50}
           fill
           alt={name + "thumbnail"}
         />
       </div>
-      <h1>{name}</h1>
+      <h1 className={css.card__name}>{name}</h1>
     </Link>
   );
 };
--- pages/galleries/[slug].tsx
+++ pages/galleries/[slug].tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { createClient } from "contentful";
+import type { GetStaticPaths, GetStaticProps } from "next";
 
-const GalleryPage = () => {
-  return <div>GalleryPage</div>;
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: "gallery",
+  });
+
+  const paths = res.items.map((item: any) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }: any) => {
+  const { items } = await client.getEntries({
+    content_type: "gallery",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: {
+      gallery: items[0],
+    },
+  };
+};
+
+const GalleryPage = ({ gallery }: any) => {
+  const { name, images } = gallery.fields;
+
+  return <div>{name}</div>;
 };
 
 export default GalleryPage;
