33b6163f437ad308d5d88b479d71f7b5ee71136a
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-12-18 17:44:32 +0000

    fix: add types for gallery card props

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,15 +3,34 @@ import Image from "next/image";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { StyledCard } from "./Styled/Card.style";
 
-const GalleryCard = ({ gallery, image }: any) => {
+interface Props {
+  gallery: {
+    imagesCollection: {
+      items: Images;
+    };
+    name: string;
+    order: number;
+    slug: string;
+  };
+}
+
+interface Image {
+  name: string;
+  url: string;
+}
+
+type Images = Image[];
+
+const GalleryCard: React.FC<Props> = ({ gallery }) => {
   const { name, order, slug } = gallery;
+  const Thumbnail = gallery.imagesCollection.items[0].url;
 
   return (
     <StyledCard>
       <Link href={"/galleries/" + slug}>
         <Image
           className="image"
-          src={image}
+          src={Thumbnail}
           quality={50}
           fill
           alt={name + "thumbnail"}
