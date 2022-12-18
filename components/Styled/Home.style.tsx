52760a8f4584e97c32c1eea9b1e8e1a237257e7c
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-12-18 19:09:27 +0000

    style: home mobile image thumbnail placement

--- components/Styled/Home.style.tsx
+++ components/Styled/Home.style.tsx
@@ -38,16 +38,35 @@ export const StyledHome = styled.section`
   @media (max-width: 420px) {
     padding: 2rem 0;
 
+    div {
+      margin: 0;
+    }
+
     div:nth-child(1) {
-      width: 300px;
-      height: 400px;
       margin-left: auto;
     }
+
     div:nth-child(2) {
-      width: 100px;
-      height: 100px;
-      margin-left: 0;
-      margin-top: 4rem;
+      margin-top: 5rem;
+    }
+
+    div:nth-child(3) {
+      margin-top: 10rem;
+      margin-left: auto;
+    }
+
+    div:nth-child(4) {
+      margin-top: 10rem;
+    }
+
+    div:nth-child(5) {
+      margin-top: 5rem;
+      margin-left: 0rem;
+    }
+
+    div:nth-child(6) {
+      margin-top: 5rem;
+      margin-left: auto;
     }
   }
 `;
