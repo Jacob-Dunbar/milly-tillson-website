b52312f1233c5bf66a5b1c2f45584f8f1574dd59
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-12-19 15:06:39 +0000

    feat: add about page

---
+++ components/About.tsx
@@ -0,0 +1,14 @@
+import { StyledAbout } from "./Styled/About.style";
+
+const About: React.FC = () => {
+  return (
+    <StyledAbout>
+      <p>
+        munich based still life and fashion photographer specialising in art
+        direction, e-commerce and styling.
+      </p>
+    </StyledAbout>
+  );
+};
+
+export default About;
