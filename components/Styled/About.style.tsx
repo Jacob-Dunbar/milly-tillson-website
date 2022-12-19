9af38271abec30eabb73699a0d88c26938985a8f
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-12-19 13:04:26 +0000

    style: about page for mobile

--- components/Styled/About.style.tsx
+++ components/Styled/About.style.tsx
@@ -20,4 +20,24 @@ export const StyledAbout = styled.section`
     line-height: 2.5rem;
     font-size: 0.8rem;
   }
+
+  @media (max-width: 420px) {
+    min-height: 80vh;
+
+    p:nth-child(1) {
+      font-size: 0.75rem;
+      margin-top: 0;
+      width: 80%;
+      text-align: center;
+      line-height: 2.5rem;
+    }
+    p:nth-child(2) {
+      margin-top: 5rem;
+      width: 300px;
+      text-align: center;
+      line-height: 2.5rem;
+      font-size: 0.5rem;
+      padding-bottom: 5rem;
+    }
+  }
 `;
