a4e980655273d6c0e8c6114d483d723bdd890743
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-12-19 15:45:03 +0000

    fix: change type.ts

--- types.ts
+++ types.ts
@@ -1,8 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      space: string | undefined;
-      accessToken: string;
+      space: any;
+      accessToken: any;
     }
   }
 }
